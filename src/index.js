baee8cc11042d0b81bcd866953b2dcdc1b941a8d
Cole Murphy <cole@spell.run>
2018-10-12 15:34:39 -0400

    Reorganize and add dependencies

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import './styles/index.scss';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+import * as serviceWorker from './utils/serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
